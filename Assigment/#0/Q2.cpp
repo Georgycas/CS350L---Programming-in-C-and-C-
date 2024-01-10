67ada9079b93d269548ed05e3efaf5d09281f61e
Georgycas <32694172+Georgycas@users.noreply.github.com>
2024-01-10 14:07:48 -0800

    Initial commit

---
+++ Assigment/#0/Q2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int Q2() {
+  int inches;
+
+  cout << "Enter the number of inches on a side " << endl;
+  cout << "Press the return key." << endl;
+  cin >> inches;
+  cout << endl << "The area is " << inches * inches << "." << endl;
+
+  return 0;
+}
